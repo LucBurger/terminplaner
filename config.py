b1f4aa5f76d3124c5d5f99530b92c1a7746512f0
Luc Burger <aburgerl@JMG.LOCAL>
2023-03-23 21:32:12 +0100

    23.03.23 Version 1.1

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'mysql+pymysql://root:terminplaner1@localhost/microblog' or \
+        'mysql+pymysql://luc:terminplaner1@localhost/microblog' or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
